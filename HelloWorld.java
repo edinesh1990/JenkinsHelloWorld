e5a1d6fab0dd4951c267bdf337e2ad4f91ae6514
Dinesh Mudaliar <edinesh.90@gmail.com>
2020-06-07 13:27:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
     public static void main (String[] args){
-        System.out.println("-----**********MADE SOME CHANGES**********------");
+        System.out.println("-----**********Jenkins HelloWorld with GitHub hook using Git SCM polling**********------");
     }
 }
